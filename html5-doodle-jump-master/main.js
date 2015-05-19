13a3370978fc4b12035427a40bb23ea938af94a5
WilliamWKcook <william_cook2004@yahoo.co.uk>
2015-05-19 17:04:31 +0800

    tehte
    hthte

--- html5-doodle-jump-master/main.js
+++ html5-doodle-jump-master/main.js
@@ -20,7 +20,12 @@ var platforms = [],
   image = document.getElementById("sprite"),
   player, platformCount = 10,
   position = 0,
-  gravity = 0.2,
+  gravity = 0.1
+
+
+
+
+ ,
   animloop,
   flag = 0,
   menuloop, broken = 0,
@@ -139,11 +144,13 @@ function Platform() {
   //3: Breakable (Go through)
   //4: Vanishable 
   //Setting the probability of which type of platforms should be shown at what score
-  if (score >= 5000) this.types = [2, 3, 3, 3, 4, 4, 4, 4];
-  else if (score >= 2000 && score < 5000) this.types = [2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4];
-  else if (score >= 1000 && score < 2000) this.types = [2, 2, 2, 3, 3, 3, 3, 3];
+  if (score >= 5000) this.types = [2, 3, 3, 4, 4, 4, 4, 4];
+  else if (score >= 2000 && score < 5000) this.types = [1,1
+  ];
+  else if (score >= 100
+ && score < 2000) this.types = [2, 2, 2, 3, 3, 3, 3, 3];
   else if (score >= 500 && score < 1000) this.types = [1, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3];
-  else if (score >= 100 && score < 500) this.types = [1, 1, 1, 1, 2, 2];
+  else if (score >= 100 && score < 500) this.types = [4, 4, 4, 4, 4, 4];
   else this.types = [1];
 
   this.type = this.types[Math.floor(Math.random() * this.types.length)];
@@ -515,7 +522,7 @@ function showGoMenu() {
   menu.style.visibility = "visible";
 
   var scoreText = document.getElementById("go_score");
-  scoreText.innerHTML = "You scored " + score + " points!";
+  scoreText.innerHTML = "You suck!!  " + score + " YOLO I WON BETTA LUCK NEXT TIME!!!! - WILLIAM!";
 }
 
 //Hides the game over menu
